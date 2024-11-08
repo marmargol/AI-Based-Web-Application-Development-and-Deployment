a9034cd920c7d51586e65583a9b9cf8ec37ad898
Marek Margol <marekmargol@odrabiamy.pl>
2024-11-08 12:18:10 +0100

    Add docstrings and improve response formatting

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Emotion Detector Application
+
+This module provides a Flask web application for detecting emotions in a given text.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +10,29 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detector():
+    """
+    Route to analyze emotions in a given text.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     detected_emotions = emotion_detector(text_to_analyze)
 
     if detected_emotions['dominant_emotion'] is None:
         response = "Invalid text! Please try again!"
     else:
-        response = f"For the given statement, the system response is 'anger': {detected_emotions['anger']}, 'disgust': {detected_emotions['disgust']}, 'fear': {detected_emotions['fear']}, 'joy': {detected_emotions['joy']} and 'sadness': {detected_emotions['sadness']}. The dominant emotion is {detected_emotions['dominant_emotion']}."
+        response = f"""
+        For the given statement, the system response is 'anger': {detected_emotions['anger']},
+         'disgust': {detected_emotions['disgust']}, 'fear': {detected_emotions['fear']}, 
+         'joy': {detected_emotions['joy']} and 'sadness': {detected_emotions['sadness']}. 
+         The dominant emotion is {detected_emotions['dominant_emotion']}.
+        """
 
     return response
 
 @app.route("/")
 def render_index_page():
+    """
+    Handles rendering of the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
